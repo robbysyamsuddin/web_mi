9721d5da47d093cecd4ac6827d8dcfe4aa81723a
robbysyamsuddin <Robbyrhg99@gmail.com>
2026-06-24 10:25:26 +0700

    fix: repair key script in package json, homeview and vite

--- src/views/LandingPage/TentangKami.vue
+++ src/views/LandingPage/TentangKami.vue
@@ -2,8 +2,8 @@
   <section id="tentang" class="py-5 bg-white text-center">
     <div class="container py-5 border rounded-4 bg-light shadow-sm" style="max-width: 800px;">
       <i class="fas fa-school text-success fa-3x mb-3"></i>
-      <h3 class="fw-bold text-dark">Visi Misi MI Raudlatul Ulum</h3>
-      <p class="text-muted m-0">Visi, misi, dan sambutan kepala sekolah sedang dalam proses penyusunan.</p>
+      <h3 class="fw-bold text-dark">Visi MI Raudlatul Ulum</h3>
+      <p class="text-muted m-0">TERWUJUDNYA MANUSIA BERIMAN, BERTAQWA, CERDAS, TERAMPIL DAN BERAKHLAQ KARIMAH </p>
       <span class="badge bg-secondary text-white mt-3 px-3 py-2">Segera Hadir</span>
     </div>
   </section>
